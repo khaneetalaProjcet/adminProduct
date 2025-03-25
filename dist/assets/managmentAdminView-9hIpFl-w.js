7ac3985be558df3aad22914674c74200e19d2d49
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-03-25 16:20:08 +0330

    build: v 0.1.35

--- dist/assets/managmentAdminView-9hIpFl-w.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,c as a,a as t,o}from"./index-Cq9FpgDN.js";const r={};function s(c,e){return o(),a("div",null,e[0]||(e[0]=[t("h1",null,"admin",-1)]))}const m=n(r,[["render",s]]);export{m as default};
