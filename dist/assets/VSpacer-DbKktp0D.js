d5240254482dec2a9f638603b07c97bff2d10531
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-03-25 19:54:06 +0330

    build: v 0.1.36

---
+++ dist/assets/VSpacer-DbKktp0D.js
@@ -0,0 +1,1 @@
+import{ar as a}from"./index-Dbwicb6H.js";const e=a("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/managmentActivityView-EgaWwC0w.js
@@ -0,0 +1,1 @@
+import{_ as t,c as n,a,o as c}from"./index-Dbwicb6H.js";const o={};function r(s,e){return c(),n("div",null,e[0]||(e[0]=[a("h1",null,"activity",-1)]))}const l=t(o,[["render",r]]);export{l as default};
