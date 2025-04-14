c8de8dd52760aec9cf7744f082dd1dc68902b9f5
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-04-14 10:25:52 +0330

    build: v 1.15.0

--- dist/assets/VSpacer-BzgxGgCd.js
+++
@@ -1,1 +0,0 @@
-import"./index-AJgElF1q.js";import{c as r}from"./createSimpleFunctional-jRaaOof9.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/VSpacer-fw4mhx2J.js
@@ -0,0 +1,1 @@
+import"./index-CCgHgMQC.js";import{c as r}from"./createSimpleFunctional-jnIe0T2z.js";const e=r("v-spacer","div","VSpacer");export{e as V};
