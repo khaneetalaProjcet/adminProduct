80f9e6984273f3c7db0dba65d35c0f3a4179fd18
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-05-22 15:45:44 +0330

    fix : change the main urls

---
+++ dist/assets/VSpacer-BdgXLqw7.js
@@ -0,0 +1,1 @@
+import"./index-Bqj5iAeq.js";import{c as r}from"./createSimpleFunctional-CuRM3cAp.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- dist/assets/VSpacer-CbDHXl_2.js
+++
@@ -1,1 +0,0 @@
-import"./index-CWAUipZr.js";import{c as r}from"./createSimpleFunctional-BAF11izm.js";const e=r("v-spacer","div","VSpacer");export{e as V};
