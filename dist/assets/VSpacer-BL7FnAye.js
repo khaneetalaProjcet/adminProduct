9e6785912aebc1c800d308677dcf19ad2f20a540
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-06-03 09:52:44 +0330

    build: otp in permissions

--- dist/assets/VSpacer-BL7FnAye.js
+++
@@ -1,1 +0,0 @@
-import"./index-COp8Femz.js";import{c as r}from"./createSimpleFunctional-B0cxyTcr.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/VSpacer-CijinCG6.js
@@ -0,0 +1,1 @@
+import"./index-BGGQp8am.js";import{c as r}from"./createSimpleFunctional-CvY-CSpH.js";const e=r("v-spacer","div","VSpacer");export{e as V};
